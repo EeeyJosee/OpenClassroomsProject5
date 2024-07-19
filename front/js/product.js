9d6b86a90b701bb9923292c8cbbbf52d171094e9
Jose Villalobos <jose.villalobos08@yahoo.com>
2024-07-19 12:36:20 -0400

    now able to add products to cart

--- front/js/product.js
+++ front/js/product.js
@@ -24,6 +24,7 @@ function insertProductDetails(product) {
 
     select = document.getElementById('colors');
 
+    // for each product color, we'll add it to the dropdown
     for (let i = 0; i < product.colors.length; i++) {
         var option = document.createElement('option');
         option.value = product.colors[i];
@@ -32,3 +33,42 @@ function insertProductDetails(product) {
     }
 }
 
+// Get access to button element, from here we'll add products to our cart
+const addCartButton = document.getElementById("addToCart");
+addCartButton.addEventListener("click", addToShoppingCart);
+
+// function to add products
+function addToShoppingCart() {
+
+    // see if an order already exists, otherwise create an empty array 
+    var cart = JSON.parse(localStorage.getItem('order'));
+    if (cart == null) { cart = []; }
+
+    const productId = id;
+    const productColor = colors.value;
+
+    // check for invalid inputs and already existing id & color cart items
+    const invalidInput = (parseInt(quantity.value) <= 0 || productColor === '');
+    const productExists = cart.find(({ id, color }) => id === productId && color === productColor);
+
+    if (invalidInput) { alert('Your order was invalid'); }
+
+    else {
+        if (productExists) {
+            productExists.quantity += parseInt(quantity.value);
+        }
+
+        // add new orders to the cart
+        else {
+            var orderDetails = {
+                id: productId,
+                color: productColor,
+                quantity: parseInt(quantity.value)
+            };
+            cart.push(orderDetails);
+        }
+
+        localStorage.setItem('order', JSON.stringify(cart));
+        alert('Order has been placed in the shopping cart');
+    }
+}
