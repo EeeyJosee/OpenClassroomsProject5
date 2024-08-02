30d26da1f1a64b88a816ae0da85e6cec3245cb48
Jose Villalobos <jose.villalobos08@yahoo.com>
2024-08-02 14:20:38 -0400

    Quantity modifier works better, still broken

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,6 +1,6 @@
 const emptyStorage = (localStorage.length == 0);
 
-if (!emptyStorage) { 
+if (!emptyStorage) {
 
     // retrieve object from local storage
     order = JSON.parse(localStorage.getItem('order', JSON.stringify(order)));
@@ -46,62 +46,88 @@ if (!emptyStorage) {
                     </div>
                 </div>
             </div>`;
-
         shoppingCart.appendChild(cartItem);
 
-        // add the total number of product per iteration
+        cartItem.querySelector('.itemQuantity').addEventListener('change', function changeQuantity($event) {
+            const id = $event.target.closest('article').dataset.id;
+            const color = $event.target.closest('article').dataset.color;
+            console.log(`id: ${id}, color: ${color}, value: ${this.value}`);
+
+            const productExists = order.find(({ id, color }) => id === id && color === color);
+            console.log(productExists);
+
+            // change both elements
+            // for (const w of order) {
+            //     console.log(w);
+            //     const productExists = order.find(({ id, color }) => id === w.id && color === w.color);
+
+            //     if (productExists) {
+            //         oldQuantity = productExists.quantity;
+            //         productExists.quantity = this.value;
+            //         localStorage.setItem('order', JSON.stringify(order));
+            //         productTotals(product, productExists.quantity, oldQuantity);
+            //     }
+            // }
+
+            // change one element
+            // const productExists = order.find(({ id, color }) => id === id && color === color);
+
+            if (productExists) {
+                oldQuantity = productExists.quantity;
+                productExists.quantity = this.value;
+                localStorage.setItem('order', JSON.stringify(order));
+                // productTotals(product, productExists.quantity, oldQuantity);
+            }
+
+        });
+
+        cartItem.querySelector('.deleteItem').addEventListener('click', function deleteItem($event) {
+            const id = $event.target.closest('article').dataset.id;
+            const color = $event.target.closest('article').dataset.color;
+            console.log(`delete item ${id} and ${color}`);
+            console.clear()
+            // TODO finish coding cart item removal from local storage
+            // localStorage.removeItem('order');
+        });
+
+
+
+        // add the totals of ALL products
         totalArticles += parseInt(quantity);
         document.getElementById('totalQuantity').innerText = totalArticles;
-        // add up the total price per iteration
-        cartTotal += product.price * parseInt(quantity);
+
+        cartTotal += product.price * parseInt(quantity)
         document.getElementById('totalPrice').innerText = cartTotal;
 
+        // total function for product modification
+        function productTotals(product, newQuantity, oldQuantity) {
+            // add up the total product
+            if (newQuantity < oldQuantity) { totalArticles += parseInt(newQuantity - oldQuantity); }
+            if (newQuantity > oldQuantity) { totalArticles += parseInt(newQuantity - oldQuantity); }
+            document.getElementById('totalQuantity').innerText = totalArticles;
 
-
-
-
-        // grab the classname for delete button as it's created
-        const allDeleteButtons = document.getElementsByClassName('deleteItem');
-
-        if (allDeleteButtons.length == order.length) {
-            
-            //TODO figure out how to delete an entry in a value
-            for (const deleteButton of allDeleteButtons) {
-                
-                deleteButton.addEventListener("click", function onClick() {
-                    console.log("delete entry");
-                    localStorage.removeItem('order');
-                });
-            }
-        }
-
-
-
-
-
-        // grab the classname for quantity modifier
-        const allQuantityButtons = document.getElementsByClassName('itemQuantity');
-
-        if (allQuantityButtons.length == order.length) {
-            //TODO figure out how to update a quantity value
-            for (const quantityButton of allQuantityButtons) {
-
-                quantityButton.addEventListener("change", function onClick() {
-
-                    const productExists = order.find(({ id, color }) => id === product._id && color === color);
-
-                    console.log(quantityButton);
-                    console.log(product._id, color);
-
-                    if (productExists) { productExists.quantity = quantityButton.value}
-                    localStorage.setItem('order', JSON.stringify(order));
-
-
-                });
-            }
-
+            // add up the total price
+            if (newQuantity < oldQuantity) { cartTotal += product.price * parseInt(newQuantity - oldQuantity); }
+            if (newQuantity > oldQuantity) { cartTotal += product.price * parseInt(newQuantity - oldQuantity); }
+            document.getElementById('totalPrice').innerText = cartTotal;
         }
     }
 }
 
+const firstNameInput = document.getElementById('firstName');
+const lastNameInput = document.getElementById('lastName');
+const addressInput = document.getElementById('address');
+const cityInput = document.getElementById('city');
+const emailInput = document.getElementById('email');
+const submitButton = document.getElementById('order');
 
+
+emailInput.addEventListener('input', ($event) => {
+
+})
+
+
+
+submitButton.addEventListener('click', ($event) => {
+    $event.preventDefault();
+})
